7208fa0d15f7b99046523b47e7a32c229e70bc3a
Yuvraj <yuvraj.r0810@gmail.com>
2026-03-10 19:51:12 +0530

    fs System added

---
+++ prepvault-backend/utils/fileStorage.js
@@ -0,0 +1,17 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const DATA_FILE = path.join(__dirname,'../data/experiences.json')
+
+async function readExperiences() {
+
+    const data = await fs.readFile(DATA_FILE, 'utf-8');
+    const experiences = JSON.parse(data);
+    return experiences;
+    
+}
+
+async function saveExperiences(data) {
+    const json = JSON.stringify(data, null, 2)
+    await fs.writeFile(DATA_FILE, json)
+}
