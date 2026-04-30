c9f2e2ad1f22cd413296e9a718efe967e005776a
Yuvraj <yuvraj.r0810@gmail.com>
2026-04-30 09:58:40 +0530

    fix: update socket.io cors to accept both localhost ports

--- prepvault-backend/socket.js
+++ prepvault-backend/socket.js
@@ -2,7 +2,7 @@ let ioInstance = null
 
 function init(server) {
   const { Server } = require('socket.io')
-  ioInstance = new Server(server, { cors: { origin: 'http://localhost:5174', credentials: true } })
+  ioInstance = new Server(server, { cors: { origin: ['http://localhost:5173', 'http://localhost:5174'], credentials: true } })
   ioInstance.on('connection', (socket) => {
     console.log('Socket connected:', socket.id)
     socket.on('disconnect', () => console.log('Socket disconnected:', socket.id))
